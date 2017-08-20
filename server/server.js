ae358d948fcfe6fafd577ed6bdaf27f10bb996c1
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-08-20 21:39:03 +0100

    Serve application from the server
    This commit serves as the first proper feeding of the application with all modules and resources loading properly
    
    Continues Signup

--- server/server.js
+++ server/server.js
@@ -1,39 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import path from 'path';
-import webpack from 'webpack';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackConfig from '../webpack.config';
+const path = require('path')
+const express = require('express')
 
-import routes from './routes/index';
+module.exports = {
+  app: function () {
+    const app = express();
+    const indexPath = path.join(__dirname, '../client/public/index.html');
+    const publicPath = express.static(path.join(__dirname, '../client/dist/'));
 
-const app = express();
+    app.use('/', publicPath);
+    app.get('/', function (_, res) { res.sendFile(indexPath) });
 
-const router = express.Router();
-const port = process.env.PORT || 5600;
-
-if (process.env.NODE_ENV === 'test') {
-  const compiler = webpack(webpackConfig);
-  app.use(webpackMiddleware(compiler, {
-    hot: true,
-    publicPath: webpackConfig.output.publicPath,
-    noInfo: true
-  }));
-  app.use(webpackHotMiddleware(compiler));
+    return app;
+  }
 }
-
-// parse incoming request bodies as json with body-parser
-app.use(bodyParser.json());
-// parse incoming request as querystrings also
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-routes(router);
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-app.use(router);
-
-
-export default app;
