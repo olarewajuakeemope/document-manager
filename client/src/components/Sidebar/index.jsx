060bcbe6a985ab8930d07857203f8229dba761d8
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-08-27 13:26:31 +0100

    Setup authenticated user sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Avatar from 'material-ui/Avatar';
+import ListItem from 'material-ui/List/ListItem';
+import PropTypes from 'prop-types';
+import MenuList from './MenuList';
+
 
 /**
- * Replace with appropriate info on completion
  * @class Sidebar
- * @extends {React.Component}
+ * @extends {Component}
  */
 class Sidebar extends Component {
+  /**
+   * Hook Method to ensure this runs before component mounts
+   * @param {None} none
+   * @returns {none} none
+   * @memberOf Sidebar
+   */
+  componentWillMount() {
+    this.forceUpdate();
+  }
   /**
    * @returns {Object} Jsx
    * @memberOf Sidebar
    */
   render() {
+    const { user } = this.props;
+    const isAdmin = user.roleId === 1;
+    const firstLetter = user.firstName[0].toUpperCase();
+    const secondLetter = user.lastName[0].toUpperCase();
+    let role = 'Regular-user';
+    if (isAdmin) {
+      role = 'Admin-user';
+    }
     return (
-      <div>
-        <h1>Inside Sidebar Component</h1>
+      <div className="col s3 side-nav fixed" id="slide-out">
+        <h3 className="brand-logo">acedms</h3>
+        <Link
+          id="create-doc"
+          className="btn-floating btn-large waves-effect waves-light #26a69a tooltipped"  // eslint-disable-line
+          data-position="left"
+          data-delay="50"
+          data-tooltip="new document"
+          to="/editor"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+        <div className="profile-box">
+          {isAdmin
+            ? <ListItem
+              disabled
+              leftAvatar={<Avatar>A</Avatar>}
+            />
+            : <ListItem
+              disabled
+              leftAvatar={<Avatar>{firstLetter}{secondLetter}</Avatar>}
+            />
+
+          }
+        </div>
+        <div className="divider" />
+        <MenuList role={role} isAdmin={isAdmin} user={user} />
       </div>
     );
   }
 }
 
-export default Sidebar;
+Sidebar.propTypes = {
+  user: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps)(Sidebar);
