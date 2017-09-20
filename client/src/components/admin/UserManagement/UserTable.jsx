5cdcd40c8a2d2e60a5d6b07679ecb6e59985b242
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-09-21 00:26:46 +0100

    Transform user admin table page

--- client/src/components/admin/UserManagement/UserTable.jsx
+++ client/src/components/admin/UserManagement/UserTable.jsx
@@ -9,6 +9,7 @@ import {
   TableRowColumn,
   TableFooter
 } from 'material-ui/Table';
+import { Card, CardHeader } from 'material-ui/Card';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Search } from 'semantic-ui-react';
@@ -227,89 +228,100 @@ class UserTable extends Component {
     const { offset, limit, processedData } = this.state;
 
     return (
-      <div>
-        <Search
-          loading={isLoading}
-          onSearchChange={this.handleSearchChange}
-          open={false}
-          value={value}
-        />
-        <Table className="table">
-          <TableHeader adjustForCheckbox>
-            <TableRow>
-              { tableHeaders && tableHeaders.map((header, index) => (
-                <TableHeaderColumn key={index}>
-                  <div className="rowAlign">
-                    { header.alias }
-                    { header.sortable &&
-                    <SortIcon
-                      id={header.dataAlias}
-                      className="sortIcon"
-                      onMouseUp={this.sortByColumn}
-                    />
-                    }
-                  </div>
-                </TableHeaderColumn>
-              )) }
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            stripedRows
-          >
-            {processedData.map((row, index) => (
-              <TableRow key={`${index} ${row.id}`}>
-                <TableRowColumn key={`${row.id} ${row.id}`}>
-                  {row.id}
-                </TableRowColumn>
-                <TableRowColumn key={`${row.id} ${row.email}`}>
-                  {row.email}
-                </TableRowColumn>
-                <TableRowColumn key={`${row.id} ${row.firstName}`}>
-                  {row.firstName}
-                </TableRowColumn>
-                <TableRowColumn key={`${row.id} ${row.lastName}`}>
-                  {row.lastName}
-                </TableRowColumn>
-                <TableRowColumn key={`${row.id} ${row.email}`}>
-                  <FlatButton
-                  key={`${index}flat${row.id}`} // eslint-disable-line
-                    label="Delete"
-                    secondary
-                    onTouchTap={
-                      () => {
-                        this.handleDelete(row.id, this.props.actions.deleteUser);
+      <Card
+        style={{ margin: '10px 50px' }}
+      >
+        <CardHeader
+          title="Users"
+          titleStyle={{ fontSize: '2em', color: 'white' }}
+          style={{ backgroundColor: 'rgb(0, 188, 212)', overflow: 'auto' }}
+        >
+          <span style={{ float: 'right' }}>
+            <Search
+              loading={isLoading}
+              onSearchChange={this.handleSearchChange}
+              open={false}
+              value={value}
+              style={{ display: 'inline-block', marginRight: '50px' }}
+            />
+          </span>
+        </CardHeader>
+        <div>
+          <Table className="table">
+            <TableHeader adjustForCheckbox>
+              <TableRow>
+                { tableHeaders && tableHeaders.map((header, index) => (
+                  <TableHeaderColumn key={index}>
+                    <div className="rowAlign">
+                      { header.alias }
+                      { header.sortable &&
+                      <SortIcon
+                        id={header.dataAlias}
+                        className="sortIcon"
+                        onMouseUp={this.sortByColumn}
+                      />
                       }
-                    }
-                  />
+                    </div>
+                  </TableHeaderColumn>
+                )) }
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {processedData.map((row, index) => (
+                <TableRow key={`${index} ${row.id}`}>
+                  <TableRowColumn key={`${row.id} ${row.id}`}>
+                    {row.id}
+                  </TableRowColumn>
+                  <TableRowColumn key={`${row.id} ${row.email}`}>
+                    {row.email}
+                  </TableRowColumn>
+                  <TableRowColumn key={`${row.id} ${row.firstName}`}>
+                    {row.firstName}
+                  </TableRowColumn>
+                  <TableRowColumn key={`${row.id} ${row.lastName}`}>
+                    {row.lastName}
+                  </TableRowColumn>
+                  <TableRowColumn key={`${row.id} ${row.email}`}>
+                    <FlatButton
+                    key={`${index}flat${row.id}`} // eslint-disable-line
+                      label="Delete"
+                      secondary
+                      onTouchTap={
+                        () => {
+                          this.handleDelete(row.id, this.props.actions.deleteUser);
+                        }
+                      }
+                    />
+                  </TableRowColumn>
+                </TableRow>
+              ))
+              }
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableRowColumn>
+                  <div className="footerControls">
+                    { `${Math.min((offset + 1), total)}
+                        - ${Math.min((offset + limit), total)} of ${total}` }
+                    <IconButton
+                      disabled={offset === 0}
+                      onClick={this.paginateBack}
+                    >
+                      <ChevronLeft />
+                    </IconButton>
+                    <IconButton
+                      disabled={offset + limit >= total}
+                      onClick={this.paginateForward}
+                    >
+                      <ChevronRight />
+                    </IconButton>
+                  </div>
                 </TableRowColumn>
               </TableRow>
-            ))
-            }
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableRowColumn>
-                <div className="footerControls">
-                  { `${Math.min((offset + 1), total)}
-                      - ${Math.min((offset + limit), total)} of ${total}` }
-                  <IconButton
-                    disabled={offset === 0}
-                    onClick={this.paginateBack}
-                  >
-                    <ChevronLeft />
-                  </IconButton>
-                  <IconButton
-                    disabled={offset + limit >= total}
-                    onClick={this.paginateForward}
-                  >
-                    <ChevronRight />
-                  </IconButton>
-                </div>
-              </TableRowColumn>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
+            </TableFooter>
+          </Table>
+        </div>
+      </Card>
     );
   }
 }
