0d92e7f68f50fe65cc1f41202e618b674e18defe
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-10-28 09:16:40 +0100

    Change webpack output to hardcoded bundle

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'client/dist/'),
-    filename: '[name].js',
+    filename: 'bundle.js',
     publicPath: '/'
   },
   plugins: [
